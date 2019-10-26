8bd1676757efad08d0c772554e262944bb9af23f
ManhHung94Qn <manhhung94qn@gmail.com>
2019-10-27 00:05:12 +0700

    oki

--- cookie/routes/index.js
+++ cookie/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/suadaunanh/:sdt', function(req, res, next) {
+  res.cookie('sdt', req.params.sdt);
+  res.send("Số điện thoại là: " + req.params.sdt)
+});
+
+/* GET home page. */
+router.get('/banhran', function(req, res, next) {
+  res.send("Số điện thoại là: " + req.cookies.sdt)
+});
+/* GET home page. */
+router.get('/xoa', function(req, res, next) {
+  if(req.cookies.sdt){
+    res.clearCookie('sdt');
+  }
+  res.send("Đã xóa cookie " )
+});
+
 module.exports = router;
